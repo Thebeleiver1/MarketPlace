797cd98a052bdc23e31decb3961f58f4e877f67b
punya <punyachapagain@gmail.com>
2018-05-10 23:12:40 +1000

    added validation in products table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
     belongs_to :user, :foreign_key => :sellerId, :primary_key => :id
     mount_uploaders :images, ImageUploader
-    validates :sellerId, presence: true    
+    validates :sellerId,:images,:name,:price,:location, presence: true    
 end
